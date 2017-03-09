aaef4b9b148550217ea0526ba3f61de0e8166719
Adam Pisula <adam.pisula@outlook.com>
2017-03-09 20:32:12 +0100

    ulams_spiral-winforms

---
+++ Ulam_Spiral/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Ulam_Spiral
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(698, 681);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(681, 633);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown2.Location = new System.Drawing.Point(681, 658);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(544, 681);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(148, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(605, 635);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rozmiar spirali";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 661);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Rozmiar punktu";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(503, 686);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Postęp";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(568, 656);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(22, 22);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.Blue;
+            this.button3.Location = new System.Drawing.Point(568, 631);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(22, 22);
+            this.button3.TabIndex = 8;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(509, 635);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Zwyczajna";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(516, 661);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Pierwsza";
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(655, 606);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Zapisz";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(734, 711);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Ulam_Spiral/Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ulam_Spiral
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        SolidBrush bpen, wpen;
+        int x, y;
+        int point = 2;
+        Bitmap bmp;
+
+        Color coln = Color.Blue;
+        Color colp = Color.Black;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult res1 = colorDialog1.ShowDialog();
+            if (res1 == DialogResult.OK)
+            {
+                coln = colorDialog1.Color;
+                button3.BackColor = coln;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult res2 = colorDialog2.ShowDialog();
+            if (res2 == DialogResult.OK)
+            {
+                colp = colorDialog2.Color;
+                button2.BackColor = colp;
+            }
+        }
+
+        //IMAGE
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Convert.ToInt32(numericUpDown1.Value) + 1, Convert.ToInt32(numericUpDown1.Value) + 1);
+            decimal n = numericUpDown1.Value;
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = Convert.ToInt32(n*n);
+
+            if (numericUpDown1.Value % 2 == 0)
+            {
+                x = bmp.Size.Width / 2;
+                y = bmp.Size.Height / 2;
+            }
+            else if (numericUpDown1.Value % 2 != 0)
+            {
+                x = (bmp.Size.Width + 1) / 2;
+                y = (bmp.Size.Height + 1) / 2;
+            }
+
+            bmp.SetPixel(x, y, coln);
+
+            bool ru = false;
+            int times = 1;
+
+            progressBar1.Value = times;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (ru == true)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        x -= 1;
+                        times++;
+                        if (Calc.Prime(times) == true)
+                            bmp.SetPixel(x, y, colp);
+                        else
+                            bmp.SetPixel(x, y, coln);
+                        progressBar1.Value = times;
+                    }
+
+                    for (int k = 0; k < i; k++)
+                    {
+                        y += 1;
+                        times++;
+                        if (Calc.Prime(times) == true)
+                            bmp.SetPixel(x, y, colp);
+                        else
+                            bmp.SetPixel(x, y, coln);
+                        progressBar1.Value = times;
+                    }
+
+                    ru = false;
+                }
+                else
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        x += 1;
+                        times++;
+                        if (Calc.Prime(times) == true)
+                            bmp.SetPixel(x, y, colp);
+                        else
+                            bmp.SetPixel(x, y, coln);
+                        progressBar1.Value = times;
+                    }
+
+                    for (int k = 0; k < i; k++)
+                    {
+                        y -= 1;
+                        times++;
+                        if (Calc.Prime(times) == true)
+                            bmp.SetPixel(x, y, colp);
+                        else
+                            bmp.SetPixel(x, y, coln);
+                        progressBar1.Value = times;
+                    }
+
+                    ru = true;
+                }
+            }
+            if (n % 2 != 0)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    x += 1;
+                    times++;
+                    if (Calc.Prime(times) == true)
+                        bmp.SetPixel(x, y, colp);
+                    else
+                        bmp.SetPixel(x, y, coln);
+                    progressBar1.Value = times;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    x -= 1;
+                    times++;
+                    if (Calc.Prime(times) == true)
+                        bmp.SetPixel(x, y, colp);
+                    else
+                        bmp.SetPixel(x, y, coln);
+                    progressBar1.Value = times;
+                }
+            }
+
+            bmp.Save("ulam_spiral_" + DateTime.Now.Day.ToString() + '-' + DateTime.Now.Month.ToString() + '-' + DateTime.Now.Year.ToString() + '_' + DateTime.Now.Hour.ToString() + '-' + DateTime.Now.Minute.ToString() + '-' + DateTime.Now.Second.ToString() + ".png", ImageFormat.Png);
+        }
+
+        //GRAPHICS
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            bpen = new SolidBrush(colp);
+            wpen = new SolidBrush(coln);
+            point = Convert.ToInt32(numericUpDown2.Value);
+
+            g.Clear(Color.White);
+
+            decimal n = numericUpDown1.Value;
+            progressBar1.Maximum = Convert.ToInt32(n * n);
+
+            if (point % 2 == 0)
+            {
+                x = this.Width / 2 - point / 2;
+                y = this.Height / 2 - point / 2;
+            }
+            else if (point % 2 != 0)
+            {
+                x = this.Width / 2 - (point + 1) / 2;
+                y = this.Height / 2 - (point + 1) / 2;
+            }
+            
+            g.FillRectangle(wpen, new Rectangle(x, y, point, point));
+
+            bool ru = false;
+            int times = 1;
+
+            progressBar1.Value = times;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (ru == true)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        x -= point;
+                        times++;
+                        if (Calc.Prime(times) == true)
+                            g.FillRectangle(bpen, new Rectangle(x, y, point, point));
+                        else
+                            g.FillRectangle(wpen, new Rectangle(x, y, point, point));
+                        progressBar1.Value = times;
+                        //Thread.Sleep(3);
+                    }
+
+                    for (int k = 0; k < i; k++)
+                    {
+                        y += point;
+                        times++;
+                        if (Calc.Prime(times) == true)
+                            g.FillRectangle(bpen, new Rectangle(x, y, point, point));
+                        else
+                            g.FillRectangle(wpen, new Rectangle(x, y, point, point));
+                        progressBar1.Value = times;
+                        //Thread.Sleep(3);
+                    }
+
+                    ru = false;
+                }
+                else
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        x += point;
+                        times++;
+                        if (Calc.Prime(times) == true)
+                            g.FillRectangle(bpen, new Rectangle(x, y, point, point));
+                        else
+                            g.FillRectangle(wpen, new Rectangle(x, y, point, point));
+                        progressBar1.Value = times;
+                        //Thread.Sleep(3);
+                    }
+
+                    for (int k = 0; k < i; k++)
+                    {
+                        y -= point;
+                        times++;
+                        if (Calc.Prime(times) == true)
+                            g.FillRectangle(bpen, new Rectangle(x, y, point, point));
+                        else
+                            g.FillRectangle(wpen, new Rectangle(x, y, point, point));
+                        progressBar1.Value = times;
+                        //Thread.Sleep(3);
+                    }
+
+                    ru = true;
+                }
+            }
+            if (n % 2 != 0)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    x += point;
+                    times++;
+                    if (Calc.Prime(times) == true)
+                        g.FillRectangle(bpen, new Rectangle(x, y, point, point));
+                    else
+                        g.FillRectangle(wpen, new Rectangle(x, y, point, point));
+                    progressBar1.Value = times;
+                    //Thread.Sleep(3);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    x -= point;
+                    times++;
+                    if (Calc.Prime(times) == true)
+                        g.FillRectangle(bpen, new Rectangle(x, y, point, point));
+                    else
+                        g.FillRectangle(wpen, new Rectangle(x, y, point, point));
+                    progressBar1.Value = times;
+                    //Thread.Sleep(3);
+                }
+            }
+
+            g.Dispose();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    class Calc
+    {
+        public static bool Prime(decimal a)
+        {
+            if (a == 2)
+                return true;
+
+            bool p = false;
+            decimal n = 0;
+            int x = 0;
+
+            if (a % 2 != 0)
+            {
+                n = (a + 1) / 2;
+            }
+            else
+            {
+                return false;
+            }
+
+            for (decimal i = 2; i < n + 1; i++)
+            {
+                x = Convert.ToInt32(a % i);
+
+                if (x == 0)
+                {
+                    p = false;
+                    break;
+                }
+                else
+                {
+                    p = true;
+                    continue;
+                }
+            }
+
+            return p;
+        }
+
+        public static bool Perfect(decimal a)
+        {
+            List<decimal> dividors = new List<decimal>();
+            decimal sum = 0;
+
+            if (a % 2 == 0)
+            {
+                for (int i = 1; i < a / 2 + 1; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        dividors.Add(i);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                for (int j = 0; j < dividors.Count; j++)
+                {
+                    sum += dividors[j];
+                }
+
+                dividors.Clear();
+
+                if (sum == a)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static decimal Factorial(decimal a)
+        {
+            if (a <= 0)
+                return 1;
+            return a * Factorial(a - 1);
+        }
+
+        public static decimal Power(decimal a, decimal b)
+        {
+            decimal result = 1;
+
+            for (int i = 0; i < b; i++)
+                result *= a;
+
+            return result;
+        }
+    }
+}
